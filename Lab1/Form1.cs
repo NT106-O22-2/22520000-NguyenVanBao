1d6f8077350b612785f39909a0e049ddffa3c6d6
Bao Nguyen <nvbao45@gmail.com>
2024-03-11 08:26:22 +0700

    lab1: demo

---
+++ Lab1/Form1.cs
@@ -0,0 +1,17 @@
+namespace Lab1
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			string fullname = tbFullname.Text;
+
+			MessageBox.Show(string.Format("Chao mung ban {0}", fullname));
+		}
+	}
+}
